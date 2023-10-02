431d9009476239e3ed1b90abe2f7cd7ecb3c7103
Shounak Naik <shounak.naik.official@gmail.com>
2023-10-02 21:24:43 +0530

    Training code

---
+++ Training_code/Higgs_to_bb.py
@@ -0,0 +1,140 @@
+import keras
+import numpy as np
+import tables
+from keras.models import Model
+from keras.layers import Input, Dense, BatchNormalization
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+# 27 features
+features = ['fj_jetNTracks',
+            'fj_nSV',
+            'fj_tau0_trackEtaRel_0',
+            'fj_tau0_trackEtaRel_1',
+            'fj_tau0_trackEtaRel_2',
+            'fj_tau1_trackEtaRel_0',
+            'fj_tau1_trackEtaRel_1',
+            'fj_tau1_trackEtaRel_2',
+            'fj_tau_flightDistance2dSig_0',
+            'fj_tau_flightDistance2dSig_1',
+            'fj_tau_vertexDeltaR_0',
+            'fj_tau_vertexEnergyRatio_0',
+            'fj_tau_vertexEnergyRatio_1',
+            'fj_tau_vertexMass_0',
+            'fj_tau_vertexMass_1',
+            'fj_trackSip2dSigAboveBottom_0',
+            'fj_trackSip2dSigAboveBottom_1',
+            'fj_trackSip2dSigAboveCharm_0',
+            'fj_trackSipdSig_0',
+            'fj_trackSipdSig_0_0',
+            'fj_trackSipdSig_0_1',
+            'fj_trackSipdSig_1',
+            'fj_trackSipdSig_1_0',
+            'fj_trackSipdSig_1_1',
+            'fj_trackSipdSig_2',
+            'fj_trackSipdSig_3',
+            'fj_z_ratio']
+
+# spectators to define mass/pT window
+spectators = ['fj_sdmass',
+              'fj_pt']
+
+# 2 labels: QCD or Hbb
+labels = ['fj_isQCD*sample_isQCD',
+          'fj_isH*fj_isBB']
+
+nfeatures = len(features)
+nspectators = len(spectators)
+nlabels = len(labels)
+
+def get_features_labels(file_name, remove_mass_pt_window=True):
+    # load file
+    h5file = tables.open_file(file_name, 'r')
+    njets = getattr(h5file.root,features[0]).shape[0]
+
+    # allocate arrays
+    feature_array = np.zeros((njets,nfeatures))
+    spec_array = np.zeros((njets,nspectators))
+    label_array = np.zeros((njets,nlabels))
+
+    # load feature arrays
+    for (i, feat) in enumerate(features):
+        feature_array[:,i] = getattr(h5file.root,feat)[:]
+
+    # load spectator arrays
+    for (i, spec) in enumerate(spectators):
+        spec_array[:,i] = getattr(h5file.root,spec)[:]
+
+    # load labels arrays
+    for (i, label) in enumerate(labels):
+        prods = label.split('*')
+        prod0 = prods[0]
+        prod1 = prods[1]
+        fact0 = getattr(h5file.root,prod0)[:]
+        fact1 = getattr(h5file.root,prod1)[:]
+        label_array[:,i] = np.multiply(fact0,fact1)
+
+    # remove samples outside mass/pT window
+    if remove_mass_pt_window:
+        feature_array = feature_array[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
+        label_array = label_array[(spec_array[:,0] > 40) & (spec_array[:,0] < 200) & (spec_array[:,1] > 300) & (spec_array[:,1] < 2000)]
+
+    feature_array = feature_array[np.sum(label_array,axis=1)==1]
+    label_array = label_array[np.sum(label_array,axis=1)==1]
+
+    h5file.close()
+    return feature_array, label_array
+
+# load training file
+feature_array, label_array = get_features_labels('ntuple_merged_10.h5', remove_mass_pt_window=False)
+
+# define dense keras model
+inputs = Input(shape=(nfeatures,), name = 'input')  
+x = BatchNormalization(name='bn_1')(inputs)
+x = Dense(64, name = 'dense_1', activation='relu')(x)
+x = Dense(32, name = 'dense_2', activation='relu')(x)
+x = Dense(32, name = 'dense_3', activation='relu')(x)
+outputs = Dense(nlabels, name = 'output', activation='softmax')(x)
+keras_model = Model(inputs=inputs, outputs=outputs)
+keras_model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
+print(keras_model.summary())
+
+
+# define callbacks
+early_stopping = EarlyStopping(monitor='val_loss', patience=10)
+model_checkpoint = ModelCheckpoint('keras_model_best.h5', monitor='val_loss', save_best_only=True)
+callbacks = [early_stopping, model_checkpoint]
+
+# fit keras model
+keras_model.fit(feature_array, label_array, batch_size=1024, 
+                epochs=100, validation_split=0.2, shuffle=False,
+                callbacks = callbacks)
+
+     
+# load testing file
+feature_array_test, label_array_test = get_features_labels('ntuple_merged_1.h5')
+
+# reload best weights
+keras_model.load_weights('keras_model_best.h5')
+
+# run model inference on test data set
+predict_array_test = keras_model.predict(feature_array_test)
+
+# create ROC curve
+fpr, tpr, threshold = roc_curve(label_array_test[:,1], predict_array_test[:,1])
+    
+# plot ROC curve
+plt.figure()
+plt.plot(tpr, fpr, lw=2.5, label="AUC = {:.1f}%".format(auc(fpr,tpr)*100))
+plt.xlabel(r'True positive rate')
+plt.ylabel(r'False positive rate')
+plt.semilogy()
+plt.ylim(0.001,1)
+plt.xlim(0,1)
+plt.grid(True)
+plt.legend(loc='upper left')
+plt.tight_layout()
+#plt.savefig('ROC.png')
+#plt.savefig('ROC.pdf')
+plt.show()
